36eee032506a92e16533403af10232ad89726e61
Su Wang <suw.mail@gmail.com>
2013-11-11 15:50:41 -0800

    Add styling for viewing stories as well as fuzzy search

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ var openZenApp = angular.module('openZenApp', [
     'ngRoute',
     'ngResource',
     'openZenDataServices',
+    'openZenConfigService',
 ]);
 
 openZenApp.config(['$routeProvider', '$locationProvider',
@@ -25,10 +26,36 @@ openZenApp.config(['$routeProvider', '$locationProvider',
             otherwise({
                 redirectTo: '/'
             });
-    }]);
+    }
+]);
 
-openZenApp.controller('FrontPageController', ['$scope', '$location', 'Story',
-    function($scope, $location, Story) {
-        $scope.stories = Story.get({page:'1'});
+/* Controllers */
+openZenApp.controller('FrontPageController', ['$scope', '$location', 'Story', 'AZ_URI', 'AZ_PROJECT_ID',
+    function($scope, $location, Story, AZ_URI, AZ_PROJECT_ID) {
+        $scope.isLoading = true;
+        $scope.stories = Story.get(
+            {page:'1'},
+            function () {
+                $scope.isLoading = false;
+            }
+        );
+
+        /* Currently not used */
+        $scope.phaseCheck = function(item) {
+            // If there is no filter or filter is all, don't bother filtering
+            if ($scope.phaseFilter == 'All'
+                || $scope.phaseFilter == undefined) {
+                return true;
+            }
+            return item.phase.name == $scope.phaseFilter;
+        };
+
+        $scope.getStoryLink = function(item) {
+            return AZ_URI
+            + '/project/'
+            + AZ_PROJECT_ID
+            + '/story/'
+            + item.id;
+        }
     }
 ]);
--- app/js/services.js
+++ app/js/services.js
@@ -7,10 +7,14 @@ var openZenDataServices = angular.module('openZenDataServices', [
     'openZenConfigService'
 ]);
 
-openZenDataServices.factory('Story', ['$resource', 'API_KEY', 'AZ_PROJECT_ID',
-    function($resource, API_KEY, AZ_PROJECT_ID) {
+openZenDataServices.factory('Story', ['$resource', 'API_URI', 'API_KEY', 'AZ_PROJECT_ID',
+    function($resource, API_URI, API_KEY, AZ_PROJECT_ID) {
         var Story = $resource(
-            'https://agilezen.com/api/v1/projects/' + AZ_PROJECT_ID + '/stories/:storyNum?apikey=' + API_KEY,
+            API_URI + '/projects/'
+                + AZ_PROJECT_ID
+                + '/stories/:storyNum?apikey='
+                + API_KEY
+                + '&pageSize=1000&where=not(phase:"live" or phase:"backlog")',
             {},
             {
                 query: {
