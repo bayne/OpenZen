4545cb47bf32c5df9ff538ba85aa38fef7dec337
Su Wang <suw.mail@gmail.com>
2013-11-11 15:18:20 -0800

    Add AZ_URI and PI_URI to sample config

--- app/config/sample-config.js
+++ app/config/sample-config.js
@@ -8,5 +8,7 @@
  */
 
 angular.module('OpenZenConfiguration')
+    .constant('AZ_URI', 'https://agilezen.com')
+    .constant('API_URI', 'https://agilezen.com/api/v1')
     .constant('API_KEY', 'REPLACE_ME_WITH_A_REAL_KEY')
     .constant('AZ_PROJECT_ID', 'REPLACE_ME_WITH_A_REAL_PROJECT_ID');
