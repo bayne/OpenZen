b687d39ba9c9c924d411a85e304d6b0afcc454ef
Su Wang <suw.mail@gmail.com>
2013-11-10 17:01:04 -0800

    Add sample config file and .gitignore

---
+++ app/config/sample-config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* OpenZen Configuration Module Sample */
+
+/* How to use:
+ * 1. Copy this file into config.js
+ * 2. Fill out the elements below
+ */
+
+angular.module('OpenZenConfiguration')
+    .constant('API_KEY', 'REPLACE_ME_WITH_A_REAL_KEY')
+    .constant('AZ_PROJECT_ID', 'REPLACE_ME_WITH_A_REAL_PROJECT_ID');
